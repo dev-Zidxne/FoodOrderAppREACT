c089b0468e5a6fd83f11aa2f5b0b66295b42f0db
Zidane Innis <zidaneinnis@gmail.com>
2022-07-17 10:15:17 -0500

    Added Header Component and Cart button component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { Fragment } from "react";
+import Header from "./Layout/Header";
+import HeaderCartButton from "./Layout/HeaderCartButton";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Fragment>
+        <Header />
+      </Fragment>
     </div>
   );
 }
